9a7de06493e3734199cea72b9f047381ffb5c84f
Refly Hadiwijaya <reflyh2@live.com>
2024-08-26 18:14:46 +0700

    Remove test pages

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueEasyForm from '@reflyh2/vue-easy-form'
-import './assets/tailwind.css'
-
-const app = createApp(App)
-app.use(VueEasyForm)
-app.mount('#app')
